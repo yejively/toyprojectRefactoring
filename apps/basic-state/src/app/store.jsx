f9ab0dc0c83a8e41756e2cfd43fe2da20d570a26
yejively <hoshikyj@naver.com>
2025-02-06 12:51:13 +0900

    Feat: toyproject redult toolkit 부가 설명

--- apps/basic-state/src/app/store.jsx
+++ apps/basic-state/src/app/store.jsx
@@ -4,7 +4,7 @@ import exSlice from '@/pages/Main/ui/Pages/slice';
 // 스토어 생성
 const store = configureStore({
     reducer: {
-        changeStr: exSlice, // 여러 슬라이스를 추가할 수도 있음
+        changeStr: exSlice, // key값 - slice의 name, value값 - 해당 리듀서에 맞게 임의 작성.
     },
 });
 
--- apps/basic-state/src/pages/Main/ui/Pages/slice.jsx
+++ apps/basic-state/src/pages/Main/ui/Pages/slice.jsx
@@ -1,18 +1,31 @@
 import { createSlice } from '@reduxjs/toolkit';
-
+// createSlice는 내부적으로 immer 라이브러리를 사용한다. -> 기존 객체를 직접적으로 수정해도 불변성 유지로 이전 상태를 변경하지않고 새로운 상태를 생성.
 const initialState = {
     str: '',
 };
 
 const exSlice = createSlice({
-    name: 'changeStr',
+    name: 'changeStr', // slice 식별 이름
     initialState,
     reducers: {
         one: state => {
-            state.str = '1';
+            state.str = '1'; // 상태를 직접적으로 변경해도 새로운 객체가 생성됨.
         },
     },
 });
+
+// const tokenSlice = createSlice({
+//     name: 'token',
+//     initialState: { tokenList: {}, tokenSet: {} },
+//     reducers: {
+//         setTokenList: (state, action) => {
+//             state.tokenList = action.payload;
+//         },
+//         setTokenDataset: (state, action) => {
+//             state.tokenSet = action.payload;
+//         },
+//     },
+// });
 
 export const { one } = exSlice.actions;
 export default exSlice.reducer;
